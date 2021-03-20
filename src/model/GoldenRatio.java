645903552982074f4ba03162481a33e159b83dcd
Eugene Gusarov <2000admiral@mail.ru>
2021-03-20 15:19:29 +0300

    Fix GoldenRatio

--- src/model/GoldenRatio.java
+++ src/model/GoldenRatio.java
@@ -5,19 +5,23 @@ public class GoldenRatio extends Method {
 
     @Override
     public double calculate() {
-        double x1 = left + (right - left) / (phi + 1),
+        double x1 = right - (right - left) / phi,
                 y1 = Function.calculate(x1),
-                x2 = right - (right - left) / (phi + 1),
+                x2 = left + (right - left) / phi,
                 y2 = Function.calculate(x2);
         while (Math.abs(right - left) / 2 > preciseness) {
             if (y1 < y2) {
                 right = x2;
                 x2 = x1;
-                x1 = left + (right - left) / (phi + 1);
+                y2 = y1;
+                x1 = right - (right - left) / phi;
+                y1 = Function.calculate(x1);
             } else {
                 left = x1;
                 x1 = x2;
-                x2 = right - (right - left) / (phi + 1);
+                y1 = y2;
+                x2 = left + (right - left) / phi;
+                y2 = Function.calculate(x2);
             }
         }
         return (left + right) / 2;
